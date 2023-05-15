b96dd98f9715a110037e32d1fb755517912daf20
Oskina Margarita <rita_2113@mail.ru>
2023-05-16 02:07:31 +0300

    Changes

---
+++ src/test/java/ru/netology/Packages/servises/CountMonthServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.Packages.servises;
+import org.junit.jupiter.api.Test;
+
+public class CountMonthServiceTest {
+
+        @Test
+
+        void shouldCalculateHolidays(int count) {
+            CountMonthService service = new CountMonthService;
+
+
+            int income = 10_000;
+            int expenses = 3_000;
+            int threshold = 20_000;
+
+            System.out.println(count);
+
+        }
+    }
