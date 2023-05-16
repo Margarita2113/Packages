c2de566f7de6a62eb424df201d0f625f76e28990
Oskina Margarita <rita_2113@mail.ru>
2023-05-16 11:30:44 +0300

    Changes 2

--- src/main/java/ru/netology/Packages/servises/CountMonthService.java
+++ src/main/java/ru/netology/Packages/servises/CountMonthService.java
@@ -2,16 +2,16 @@ package ru.netology.Packages.servises;
 
 class CountMonthService {
     public int calculate(int income, int expenses, int threshold) {
+
         int count = 0; // счетчик месяцев отдыха
         int money = 0; // количество денег на счету
 
-
-        for (int month =0; month <12; month++) {
+        for (int month = 0; month < 12; month++) {
             if (money >= threshold) {
                 count++;
-                money = (money - expenses)/3;
-            }else {
-                money = money + income - expenses;
+                money = (money - expenses) / 3;
+            } else {
+                money = (money + income) - expenses;
             }
         }
         return count;
--- src/test/java/ru/netology/Packages/servises/CountMonthServiceTest.java
+++ src/test/java/ru/netology/Packages/servises/CountMonthServiceTest.java
@@ -1,19 +1,25 @@
 package ru.netology.Packages.servises;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CountMonthServiceTest {
 
-        @Test
+    @Test
+    void calculateHolidays() {
 
-        void shouldCalculateHolidays(int count) {
-            CountMonthService service = new CountMonthService;
+        CountMonthService service = new CountMonthService();
+
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+
+        int actual = service.calculate(income, expenses, threshold);
+        int expected = 3;
 
 
-            int income = 10_000;
-            int expenses = 3_000;
-            int threshold = 20_000;
-
-            System.out.println(count);
-
-        }
+        Assertions.assertEquals(expected, actual);
     }
+
+
+}
